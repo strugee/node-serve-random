67b27743c748b18c8d7808b7c31408b9fc56a2dd
AJ Jordan <alex@strugee.net>
2017-06-24 01:04:22 -0700

    Implement module

--- README.md
+++ README.md
@@ -2,7 +2,27 @@
 
 Serve random files
 
-Somewhat API-compatible with `serve-static`.
+Vaguely API-compatible with [serve-static][].
+
+## API
+
+### serveRandom(root, options)
+
+Create a new middleware function to serve files from within a given root directory. With each inbound request, a random file will be selected from the given root directory. The module will not recurse into subdirectories.
+
+The response's max-age is always set to 0.
+
+#### Options
+
+##### fallthrough
+
+Set the middleware to have client errors (mostly just use of an HTTP method other than `GET` or `HEAD`) fall-through as just unhandled requests, otherwise forward a client error. The difference is that client errors will cause this middleware to simply `next()` to your next middleware when this value is `true`. When this value is `false`, these errors (even 404s), will invoke `next(err)`.
+
+Defaults to `true`.
+
+## Acknowledgments
+
+This module heavily borrows code from [serve-static][] - many thanks to the great people who work on that module.
 
 ## License
 
@@ -11,3 +31,5 @@ LGPL 3.0+
 ## Author
 
 AJ Jordan <alex@strugee.net>
+
+ [serve-static]: https://github.com/expressjs/serve-static
---
+++ index.js
@@ -0,0 +1,71 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict';
+
+var resolve = require('path').resolve,
+    fs = require('fs'),
+    send = require('send');
+
+module.exports = function serveRandom(root, opts) {
+	var opts = Object.create(opts || null),
+	    fallthrough = opts.fallthrough || true;
+
+	if (!root) {
+		throw new TypeError('root path required');
+	}
+
+	if (typeof root !== 'string') {
+		throw new TypeError('root path must be a string');
+	}
+
+	return function serveStatic(req, res, next) {
+		if (req.method !== 'GET' && req.method !== 'HEAD') {
+			if (fallthrough) {
+				next();
+				return;
+			}
+
+			// Method not allowed
+			res.statusCode = 405;
+			res.setHeader('Allow', 'GET, HEAD');
+			res.setHeader('Content-Length', '0');
+			res.end();
+			return;
+		}
+
+		fs.readdir(root, function(err, files) {
+			var path = files.filter(function(name) {
+				return name[0] !== '.';
+			// https://stackoverflow.com/a/4550514/1198896
+			})[Math.floor(Math.random() * files.length)];
+
+			var stream = send(req, path, {
+				maxAge: 0,
+				root: resolve(root),
+				lastModified: false,
+				etag: false
+			});
+
+			// Forward errors
+			stream.on('error', function error (err) {
+				next(err);
+				return;
+			});
+
+			stream.pipe(res);
+		});
+	};
+}
